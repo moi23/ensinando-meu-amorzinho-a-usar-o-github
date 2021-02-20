72bd549cd905e7004fbea7eadcc9045c4bc23b12
moi23 <moisesboas@gmail.com>
2021-02-20 16:23:03 -0300

    add: enviando meu primeiro arquivo pra nuvem XD

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('Hello Github');
+
+// COMMANDOS *
+
+// git status: para ver os arquivos que eu alterei
+// e não foi enviado ao repositorio (cloud)
+
+// git add nome aquivo // git add * para subir todos arqs
+
+// git commit -m "enviando meu primeiro arquivo //"
+
+// git push
+
+// git add *
+// git commit -m "comentario"
+// git push
